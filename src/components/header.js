508e56ee0766e617e6bf9dd596a7c5301c3772e8
Ryan Willpower <surasak.c.work@gmail.com>
2018-03-02 14:59:22 +0700

    add styled-component

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,33 @@
 import React from 'react'
 import Menus from './menus'
+import styled from 'styled-components'
+
+const Img = styled.img`
+    object-fit: cover;
+    -webkit-filter: blur(3px);
+    filter: blur(3px);
+    width: 100%;
+    height: auto;
+    position: relative;
+`
+
+const HeaderImg = styled.div`
+    text-align: center;
+    color: #FFF;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%)
+`
+
 
 export default () => {
     return(
         <header>
             <Menus />
-            {/* <div className="header-box">
-                <img src="/img/coding.png" alt="coding" className="header-img"/>
-                <div className="text-overlay">
+            <div className="header-box">
+                <Img src="/img/header-img.png" alt="header-img" className="header-img"/>
+                <HeaderImg>
                     <span>Ryan Willpower</span>
                     <br />
                     <span>Web developer</span>
@@ -15,8 +35,8 @@ export default () => {
                     <span>
                         The student at somewhere university who love programming so much.
                     </span>
-                </div>
-            </div> */}
+                </HeaderImg>
+            </div>
         </header>
     )
 }
